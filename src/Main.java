5ac571b0e0a3648954b8df488313c832424db816
Zelioforest <veshky@mail.ru>
2023-11-08 13:40:41 +0300

    inition commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int days = 10; // изначальное количество дней в Париже
+
+        if (days == 0) {
+            System.out.println("Желаем приятной поездки!");
+        } else {
+            while( days > 0) {
+                System.out.println("До конца поездки осталось " + days + " дней.");
+                days--;
+            }
+            System.out.println("Путешествие окончено. Пора возвращаться домой.");
+        }
+    }
+}
